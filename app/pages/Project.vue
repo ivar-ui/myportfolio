fddad1905a29b57300570d752550aa1a9f59b3d5
ivar-ui <ivanarya990@gmail.com>
2025-06-09 17:16:07 +0700

    Update Project.vue

--- app/pages/Project.vue
+++ app/pages/Project.vue
@@ -221,7 +221,7 @@ const handleReveal = () => {
 const projects = [
   { title: 'Donasi App (Flutter + PHP)', description: 'Merancang backend aplikasi mobile Fundraising menggunakan Flutter yang terintegrasi dengan website Fundraising berbasis laravel.', tags: ['Flutter', 'Laravel', 'MySQL'], image: '/projects/donasi.jpg' },
   { title: 'Rent Website', description: 'Merancang backend website rental antara customer dan admin menggunakan PHP, MySql yang memiliki fitur CRUD.', tags: ['Html', 'Php', 'Bootstrap'], image: '/projects/rent.jpg' },
-  { title: 'IoT Garbage Filtering Sistem', description: 'Alat makan otomatis berbasis IoT yang dapat dipantau lewat aplikasi mobile.', tags: ['Arduino', 'C++', 'Php'], image: '/projects/iot.jpg' },
+  { title: 'IoT Garbage Filtering Sistem', description: 'Merancang dan mengembangkan sistem IoT yang dapat diakses melalui website menggunakan database yang telah terintegrasi dengan arduino uno.', tags: ['Arduino', 'C++', 'Php'], image: '/projects/iot.jpg' },
 ]
 
 const multimedia = [
